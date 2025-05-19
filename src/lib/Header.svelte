7ece228c557d91d01a381b672bef5d6cd1921749
Kuttesch <moritzkuttesch@outlook.de>
2025-05-19 16:19:42 +0200

    Addition of lightmode/darkmode toogle

---
+++ src/lib/Header.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import ToggleTheme from "./ToggleTheme.svelte";
+</script>
+
+
+<div class="w-auto h-10 fixed top-0 right-0 flex justify-start items-center m-4 md:mr-8 mr-2 z-50">
+    <ToggleTheme/>
+</div>
